66dc555a4df3a623dac6e617f14de4fd77387c4e
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-11-09 15:38:06 +0100

    it works

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const noButton = document.querySelector('.no-button');
+const noButton2 = document.querySelector('.no-button');
 const canvas = document.querySelector('body')
 
 canvas.addEventListener('mousemove', (e) => {
@@ -9,26 +9,11 @@ canvas.addEventListener('mousemove', (e) => {
   let mouseCords = mouseX + mouseY;
 })
 
+noButton2.addEventListener('mouseover', (e) => {
+  let randomY = Math.floor(Math.random(window.innerHeight) * 400 - 300);
+  let randomX = Math.floor(Math.random(window.innerWidth) * 300 - 200);
 
+  e.target.style.transform = 'translateX(' + randomX+'px)' + 'translateY(' + randomY+'px)';
 
-
-
-// let test = false;
-noButton.addEventListener('mouseover', (e) => {
-  let randomY = Math.floor(Math.random(window.innerHeight) *  (window.innerHeight - 100 + 'px') - 100 + 'px');
-  let randomX = Math.floor(Math.random(window.innerWidth) * (window.innerWidth - 100 + 'px') - 100 + 'px');
-  // let random = randomX + randomY;
-
-
-  // console.log(random);
-  // let buttonY = e.clientY;
-  // let buttonX = e.clientX;
-  // let buttonCords = buttonX + buttonY;
-  console.log(e.target.style.transform = 'translateX(' + randomX+'px)' + 'translateY(' + randomY+'px)');
-  // console.log(e.target.style.transform = );
-
-  // e.target.style.transform = `'transform: translateY(${randomY}px) '`
   e.target.style.transform = `${random ? 'translateX(' + randomX + 'px)' : 'translateX(' + randomX + 'px)'}` + `${random ? 'translateY(' + randomY + 'px)' : 'translateY(' + randomY + 'px)'}`;
-
-  // test = !test;
 })
