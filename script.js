df5a278b2bff3b307b739657ea9eeae879b8d9a7
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-11-09 13:32:12 +0100

    tried some stuff

--- script.js
+++ script.js
@@ -11,12 +11,19 @@ canvas.addEventListener('mousemove', (e) => {
 
 
 noButton.addEventListener('mouseover', (e) => {
-  let buttonY = e.clientY;
-  let buttonX = e.clientX;
-  let buttonCords = buttonX + buttonY;
+  for(let i = 0; i < e; i++){
+     Math.random();
+  }
+  return (e);
 })
 
-if (canvas === noButton) {
-  noButton.target.style.transform = 'translateX(150px)';
-  noButton.target.style.width= '10px';
-}
+// noButton.addEventListener('mouseover', (e) => {
+//   let buttonY = e.clientY;
+//   let buttonX = e.clientX;
+//   let buttonCords = buttonX + buttonY;
+// })
+
+// if (canvas === noButton) {
+//   noButton.target.style.transform = 'translateX(150px)';
+//   noButton.target.style.width= '10px';
+// }
