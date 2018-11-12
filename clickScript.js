6fae02f6de3cf102a9423df465004eb55f1cadf2
Izabella <izzwii.izabella@gmail.com>
2018-11-12 12:23:12 +0100

    fixed the reload button

--- clickScript.js
+++ clickScript.js
@@ -5,11 +5,13 @@ const yesButton = document.querySelector('.yes-button');
 const noButton = document.querySelector('.no-button');
 const gif = document.querySelector('.gif');
 const h2 = document.querySelector('h2');
+const resetBth = document.querySelector('.reset')
 
 yesButton.addEventListener('click', (e) => {
 
   gif.classList.add('active');
   h2.classList.add('move');
+  resetBth.classList.add('active');
 
 })
 
