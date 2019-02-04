b5ccd29b671612140d742edf429337edc4f27519
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-02-04 10:36:05 +0100

    added reset button on no click

--- beertime.js
+++ beertime.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 const yesButton = document.querySelector('.yes-button');
 const noButton = document.querySelector('.no-button');
 const gif = document.querySelector('.gif');
@@ -19,4 +18,5 @@ noButton.addEventListener('click', (e) => {
 
   gif.classList.add('active');
   h2.classList.add('move');
+  resetBth.classList.add('active');
 })
